421d8a10c76a06fffeef821ff2c36b769a0721db
Neven Hsu <space.man0318@gmail.com>
2023-02-01 17:14:05 +0800

    contract: add pullPayment, ownable

--- contracts/CH.sol
+++ contracts/CH.sol
@@ -3,19 +3,25 @@ pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
+import '@openzeppelin/contracts/security/PullPayment.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 import 'operator-filter-registry/src/DefaultOperatorFilterer.sol';
 
-contract CH is ERC721, DefaultOperatorFilterer {
+contract CH is ERC721, Ownable, PullPayment, DefaultOperatorFilterer {
   using Counters for Counters.Counter;
   Counters.Counter private currentTokenId;
+
+  /// @dev Base token URI used as a prefix by tokenURI().
+  string public baseTokenURI = '';
+  uint256 totalSupply = 0;
 
   constructor() ERC721('ChineseNFT', 'CH') {}
 
   function mintTo(address recipient) public returns (uint256) {
     currentTokenId.increment();
-    uint256 newItemId = currentTokenId.current();
-    _safeMint(recipient, newItemId);
-    return newItemId;
+    totalSupply = currentTokenId.current();
+    _safeMint(recipient, totalSupply);
+    return totalSupply;
   }
 
   function setApprovalForAll(address operator, bool approved)
@@ -57,5 +63,25 @@ contract CH is ERC721, DefaultOperatorFilterer {
     bytes memory data
   ) public override onlyAllowedOperator(from) {
     super.safeTransferFrom(from, to, tokenId, data);
+  }
+
+  /// @dev Returns an URI for a given token ID
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseTokenURI;
+  }
+
+  /// @dev Sets the base token URI prefix.
+  function setBaseTokenURI(string memory _baseTokenURI) public onlyOwner {
+    baseTokenURI = _baseTokenURI;
+  }
+
+  /// @dev Overridden in order to make it an onlyOwner function
+  function withdrawPayments(address payable payee)
+    public
+    virtual
+    override
+    onlyOwner
+  {
+    super.withdrawPayments(payee);
   }
 }
