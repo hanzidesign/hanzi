c373470ebf1093b0c357076547db9f70b5d8c8ad
Neven Hsu <space.man0318@gmail.com>
2023-03-29 23:31:14 +0800

    switch chain

--- hooks/useAccount.ts
+++ hooks/useAccount.ts
@@ -1,19 +1,10 @@
 import { useEffect } from 'react'
-import { watchAccount, getAccount, switchNetwork } from '@wagmi/core'
-import { goerli } from 'wagmi/chains'
+import { watchAccount, getAccount } from '@wagmi/core'
 import { useAppDispatch } from 'store'
 import { setAccount } from 'store/slices/nft'
 
 export default function useAccount() {
   const dispatch = useAppDispatch()
-
-  const handleSwitch = async () => {
-    try {
-      await switchNetwork({ chainId: goerli.id })
-    } catch (error) {
-      console.error(error)
-    }
-  }
 
   useEffect(() => {
     const { address } = getAccount()
@@ -24,7 +15,6 @@ export default function useAccount() {
       dispatch(setAccount(address))
       if (address) {
         console.log(`Connect with ${address}`)
-        handleSwitch()
       }
     })
     return unwatch
--- hooks/useChain.ts
+++ hooks/useChain.ts
@@ -1,4 +1,6 @@
 import { useNetwork } from 'wagmi'
+import { switchNetwork } from '@wagmi/core'
+import { goerli } from 'wagmi/chains'
 import { useEffect } from 'react'
 import { useAppDispatch } from 'store'
 import { setChainId } from 'store/slices/nft'
@@ -8,11 +10,23 @@ export default function useChain() {
   const { chain } = useNetwork()
   const etherscanUrl = getEtherscanUrl(chain?.id)
 
+  const handleSwitch = async () => {
+    try {
+      await switchNetwork({ chainId: goerli.id })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   useEffect(() => {
     if (chain) {
       const { id: chainId } = chain
       const etherscan = getEtherscanUrl(chainId)
       dispatch(setChainId({ chainId, etherscan }))
+
+      if (chain.id !== goerli.id) {
+        handleSwitch()
+      }
     }
   }, [chain])
 
@@ -26,7 +40,7 @@ function getEtherscanUrl(id?: number) {
   switch (id) {
     case 1:
       return 'https://etherscan.io'
-    case 5:
+    case goerli.id:
       return 'https://goerli.etherscan.io'
     default:
       return 'https://optimistic.etherscan.io'
