857ea0ccd2a8d27d9197c5023d18adb26bc604cf
Neven Hsu <space.man0318@gmail.com>
2023-03-29 22:14:27 +0800

    useProgress

--- hooks/useProgress.ts
+++ hooks/useProgress.ts
@@ -3,38 +3,37 @@ import { useState, useEffect } from 'react'
 import { useInterval } from '@mantine/hooks'
 import type { Job } from 'types'
 
+const max = 96
+
 export default function useProgress(job?: Job) {
   const { startAt, ipfsUrl, failed } = job || {}
   const [progress, setProgress] = useState(0)
 
   const interval = useInterval(() => {
-    if (startAt && progress < 100) {
-      setProgress((state) => {
-        const v = state + _.random(6, 10)
-        const p = v > 100 ? 100 : v
-
-        if (p >= 100 || ipfsUrl) {
-          interval.stop()
-        }
-
-        return p
-      })
-    }
+    setProgress((state) => {
+      const v = state + _.random(6, 10)
+      const p = v > max ? max : v
+      return p
+    })
   }, 1000)
 
   useEffect(() => {
-    if (startAt && !ipfsUrl) {
+    if (startAt && !ipfsUrl && progress < max) {
       interval.start()
     }
-    return interval.stop
-  }, [startAt, ipfsUrl])
 
-  useEffect(() => {
-    if (failed) {
-      setProgress(0)
+    if (ipfsUrl && progress < max) {
       interval.stop()
+      setProgress(100)
     }
-  }, [failed])
+
+    if (failed && progress > 0) {
+      interval.stop()
+      setProgress(0)
+    }
+
+    return interval.stop
+  }, [startAt, ipfsUrl, failed, progress])
 
   return [progress, interval] as const
 }
