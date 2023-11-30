4ebe9062a89f501cd461dc5dbb8d4bd1598a8d70
Neven Hsu <space.man0318@gmail.com>
2023-11-30 13:52:58 +0800

    upgrade hardhat to v6

--- hardhat/tasks/check-balance.ts
+++ hardhat/tasks/check-balance.ts
@@ -1,11 +1,10 @@
 import { task } from 'hardhat/config'
-import { ethers } from 'ethers'
-import { getAccount, getProvider } from './helpers'
 
-task('check-balance', 'Prints out the balance of your account', async function (taskArguments, hre, runSuper) {
-  const account = await getAccount()
-  const balance = await account.provider?.getBalance(account.address)
-  const network = await getProvider().getNetwork()
+task('check-balance', 'Prints out the balance of your account', async function (taskArguments, { ethers }, runSuper) {
+  const { provider } = ethers
+  const [account] = await ethers.getSigners()
+  const balance = await provider.getBalance(account.address)
+  const network = await provider.getNetwork()
   const bal = ethers.formatEther(balance || 0)
   console.log(`Account balance for ${account.address}: ${bal} on ${network.name} (${network.chainId})`)
 })
--- hardhat/utils/calculator.ts
+++ hardhat/utils/calculator.ts
@@ -1,30 +1,16 @@
 import _ from 'lodash'
-import { BigNumber, BigNumberish } from 'ethers'
+import { toBigInt, BigNumberish } from 'ethers'
 import Decimal from 'decimal.js'
 
 function toNum(n: BigNumberish, precision = 1) {
   const floatN = new Decimal(n.toString())
   const fixedN = floatN.mul(precision).toFixed(0) // no decimals
-  return BigNumber.from(fixedN)
+  return toBigInt(fixedN)
 }
 
 export function mul(a: BigNumberish, b: BigNumberish, precision = 100) {
   const numA = toNum(a, precision)
   const numB = toNum(b, precision)
-  return numA.mul(numB).div(precision).div(precision)
-}
-
-export function inRange(value: number, range: [number, number], margin = 0) {
-  const [lower, upper] = range.sort((a, b) => a - b)
-  return upper - margin >= value && value >= lower + margin
-}
-
-export function sum(val: BigNumberish[]) {
-  return _.reduce(
-    val,
-    (sum, n) => {
-      return sum.add(BigNumber.from(n))
-    },
-    BigNumber.from(0)
-  )
+  const p = toBigInt(precision)
+  return (numA * numB) / p / p
 }
